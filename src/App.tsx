aee250995a9288ef9072815edee8b2bcea09a4d4
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-12-31 14:37:45 +0300

    feat: Добавить темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {Button} from "./components/Button/Button";
-import './index.scss';
+import './styles/index.scss';
 import {Route, Routes} from "react-router-dom";
 import {Suspense} from "react";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Меняем тему</button>
             <Suspense fallback={<div>...Loading</div>}>
                 <Routes>
                     <Route path={'/'} element={<MainPageAsync />} />
