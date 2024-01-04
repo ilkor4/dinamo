53525cda4172e6710a89a420bd61f8b31f91870f
ilkor4 <kornelyuck.ilya@yandex.ru>
2024-01-04 12:42:35 +0400

    feat: Добавить хелпер классов

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import {Suspense} from "react";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Меняем тему</button>
             <Suspense fallback={<div>...Loading</div>}>
                 <Routes>
