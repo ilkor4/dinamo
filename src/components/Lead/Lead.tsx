180ea31b87b44fa4f64b4960becae4f675a267fb
ilkor4 <kornelyuck.ilya@yandex.ru>
2024-01-23 10:28:33 +0300

    feat: Добавить оригиналы видео

--- src/components/Lead/Lead.tsx
+++ src/components/Lead/Lead.tsx
@@ -1,6 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Lead.module.scss'
-import {newsArray} from "shared/assets/utils/constants";
+import LeadImage from "shared/assets/images/lead-image.jpg";
 import {GameBar} from "widgets/GameBar";
 import {MainNews} from "components/MainNews/MainNews";
 import {Video} from "components/Video/Video";
@@ -13,7 +13,7 @@ export const Lead = ({className}: LeadProps) => {
     return (
         <section
             className={classNames(cls.Lead, {}, [className])}
-            style={{backgroundImage: `url(${newsArray[8].newsImage})`}}
+            style={{backgroundImage: `url(${LeadImage})`}}
         >
             <Video />
             <GameBar />
